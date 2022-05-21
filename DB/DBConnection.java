80bc4e512c5983cf2345c992f26c80986c6256da
beombeom1119 <qjarl1119@naver.com>
2022-05-22 00:18:36 +0900

    "공부"

---
+++ DB/DBConnection.java
@@ -0,0 +1,28 @@
+package DB;
+
+import java.sql.*;
+
+public class DBConnection {
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/lee";
+        String userName = "root";
+        String password = "1224bear";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from person");
+
+        resultSet.next();
+        String name = resultSet.getString("name");
+        String part= resultSet.getString("part");
+        System.out.println(name);
+        System.out.println(part);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+
+}
